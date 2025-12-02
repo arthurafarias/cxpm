f850d29f5ba1420c3f2b6e62aab1351a9793b149
Arthur de Araújo Farias <arthur@afarias.org>
2025-12-02 01:37:10 +0100

    Shared object compilation already supported fixes #5

---
+++ examples/shared-object/package.cpp
@@ -0,0 +1,14 @@
+#include <Models/Project.hpp>
+#include <Models/Target.hpp>
+
+auto target0 = Models::Target()
+                   .name_set("example-shared-object")
+                   .version_set("1.0.0")
+                   .type_set("shared-library")
+                   .sources_append({"src/library.cpp"})
+                   .options_append({"-fPIC", "-fstack-protector-all"})
+                   .link_libraries_append({"m"})
+                   .include_directories_append({"src"})
+                   .create();
+
+auto project = Models::Project().add(target0).create();
---
+++ examples/shared-object/src/library.cpp
@@ -0,0 +1,8 @@
+#include <library.hpp>
+
+namespace Examples::SharedObject
+{
+    int BaseArithmetic::add(const int& a, const int& b) {
+        return a + b;
+    }
+}
