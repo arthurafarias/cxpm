dc9708fe09f58c005c09d2a653d361889d56ba17
Arthur de Araújo Farias <arthur@afarias.org>
2026-06-29 04:03:18 +0200

    Fixing gcc for the new options

--- modules/toolchains/gcc/src/toolchain.cpp
+++ modules/toolchains/gcc/src/toolchain.cpp
@@ -6,9 +6,21 @@ auto toolchain = CXPM::Models::Toolchain()
                      .include_directory_prefix_set("-I")
                      .link_directory_prefix_set("-L")
                      .link_library_prefix_set("-l")
+                     .linker_options_set({"-fPIC", "-shared"})
                      .compiler_executable_set("/usr/bin/gcc")
                      .linker_executable_set("/usr/bin/gcc")
+                     .archiver_executable_set("/usr/bin/ar")
+                     .archiver_options_set({"rcs"})
                      .language_set("c")
-                     .compiler_options_set({"-pthread"});
+                     .object_build_options_set({"-fPIC"})
+                     .compiler_options_set({"-fPIE", "-pthread"})
+                     .object_prefix_set("")
+                     .object_suffix_set(".o")
+                     .shared_object_prefix_set("lib")
+                     .shared_object_suffix_set(".so")
+                     .archive_prefix_set("lib")
+                     .archive_suffix_set(".a")
+                     .executable_prefix_set("")
+                     .executable_suffix_set("");
 
 extern "C" Toolchain *get_toolchain() { return &toolchain; }
